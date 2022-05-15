92a3d63205431515a1362d4875c99cbdf2e36d4c
elena ene <enealexandra18@stud.ase.ro>
2022-05-15 12:34:01 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const messagesRouter = require('./messagesRouter');
 
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/messages",messagesRouter);
+
+
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World again!')
-  });
-  
+
   app.listen(port, () => {
     console.log(`Cloud Computing app listening on port ${port}!`)
   });
---
+++ messagesRouter.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const mysql = require('mysql');
+const req = require('express/lib/request');
+const router = express.Router();
+const connection = require('./db');
+
+
+ router.get("/", (req, res) => {
+    connection.query("SELECT * FROM messages", (err, results) => {
+      if (err) {
+       console.log(err);
+        return res.send(err);
+      }
+  
+      return res.json({
+        messages: results,
+      });
+    });
+  });
+
+  router.get("/:id", (req, res) => {
+      const{id} = req.params;
+    connection.query(`SELECT * FROM messages where entryID = ${mysql.escape(id)}`, (err, results) => {
+      if (err) {
+       console.log(err);
+        return res.send(err);
+      }
+  
+      if(results.length==0){
+          return res.status(400).json({
+        error: "Message not found",
+          })
+      }
+
+      return res.json({
+        messages: results,
+      });
+    });
+  });
+
+
+  router.post("/", (req,res) => {
+      console.log(req.body);
+      const {
+          senderName,
+          senderMail,
+          receiverMail,
+          messageContent,
+      } = req.body;
+
+      if(!senderName || !senderMail || !receiverMail || !messageContent) {
+          return res.status(400).json({
+              error: "All fields are mandatory",
+          })
+      }
+
+      connection.query(`INSERT INTO messages(senderName, senderMail, receiverMail, messageContent) values (${mysql.escape(senderName)}, ${mysql.escape(senderMail)},${mysql.escape(receiverMail)}, ${mysql.escape(messageContent)})`, (err, results) => {
+        if (err) {
+         console.log(err);
+          return res.send(err);
+        }
+    
+        return res.json({
+            results,
+        });
+      }); 
+
+  });
+
+  router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(400).send("Bad request. Missing parametres.");
+    }
+    const queryString = `DELETE FROM messages WHERE entryID = ${mysql.escape(id)}`;
+    connection.query(queryString, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        if (results.length === 0) {
+            return res.status(404).send("Message not found.");
+        }
+        return res.json({
+            results,
+        });
+    }
+    );
+}
+);
+
+router.put("/:id", (req,res) => {
+    const { id } = req.params;
+    if (!id) {
+        // send bad request error
+        return res.status(400).send("Bad request. Missing parametres.");
+    }
+    const { senderName, senderMail, receiverMail, messageContent } = req.body;
+    if (!senderName || !senderMail || !receiverMail || !messageContent) {
+        // send bad request error
+        return res.status(400).send("Bad request. Missing parametres.");
+    }
+    const queryString = `UPDATE messages SET senderName = ${mysql.escape(senderName)}, senderMail = ${mysql.escape(senderMail)}, receiverMail = ${mysql.escape(receiverMail)}, messageContent = ${mysql.escape(messageContent)} WHERE entryID = ${mysql.escape(id)}`;
+    connection.query(queryString, (err, results) => {
+        if (err) {
+            return res.send(err);
+        }
+        if (results.length === 0) {
+            return res.status(404).send("Message not found.");
+        }
+        return res.json({
+            results,
+        });
+    }
+    );
+});
+
+module.exports = router;
